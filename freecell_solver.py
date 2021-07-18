b7baf34ee365a66a3ae2491bfb18671d3a410df0
Fuck <You>
2021-07-18 18:44:13 +0300

    - 4 suits readed

--- freecell_solver.py
+++ freecell_solver.py
@@ -7,34 +7,20 @@ from collections import defaultdict
 
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-# card string to text nominals list
-CARD_NOMINALS_LIST = ['2','3','4','5','6','7','8','9','1','J','Q','K','A']
+# card string to text values list
+RESTRICTED_VALUES_LIST = ['2','3','4','5','6','7','8','9','1','J','Q','K','A']
 
 def show():
 	#cv2.imshow('bin', bin)
 	#cv2.imshow('gray', gray)
 	#cv2.imshow('image', image)
 	#cv2.imshow('h', h)
+	#cv2.imshow('roi', roi)
 
 	plt.figure(1)
-	plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+	#plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+	plt.imshow(cv2.cvtColor(roi_suit, cv2.COLOR_BGR2RGB))
 	plt.show()
-
-	#plt.figure(2)
-	#plt.imshow(cv2.cvtColor(bin, cv2.COLOR_BGR2RGB))
-	#plt.show()
-
-	#plt.figure(3)
-	#plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
-	#plt.show()
-
-	#plt.figure(4)
-	#plt.imshow(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB))
-	#plt.show()
-
-	#plt.figure(5)
-	#plt.imshow(cv2.cvtColor(bin, cv2.COLOR_BGR2RGB))
-	#plt.show()
 
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
@@ -51,10 +37,32 @@ SYM_X_STEP = 110
 SYM_Y_STEP = 30
 
 total_cards_readed = 0
-total_red_suits = 0
 cards_dict = defaultdict(lambda:0)
+cards_suits_dict = defaultdict(lambda:0)
+readed_cards_list = []
+
+# game stacks
+CARD_VALUE_KEY = 'value'
+CARD_SUIT_KEY = 'suit'
+CARD_BUFFER_KEY = 'buffer'
+GOAL_KEY = 'goal'
+
+game = {}
+# 4 cards buffer
+game[CARD_BUFFER_KEY] = []
+# goal of the game
+game[GOAL_KEY] = {}
+game[GOAL_KEY]['spades'] = []
+game[GOAL_KEY]['clubs'] = []
+game[GOAL_KEY]['diamonds'] = []
+game[GOAL_KEY]['hearts'] = []
+
 
 for x in range(0, 8):
+
+	# game column
+	game[x] = []
+
 	for y in range(0, 7):
 		roi_x = 989 + x*SYM_X_STEP
 		roi_y = 617 + y*SYM_Y_STEP
@@ -71,114 +79,115 @@ for x in range(0, 8):
 		check_bit_y = roi_y + 80
 		check_bit = image[check_bit_y, check_bit_x]
 		if check_bit[1] > check_bit[0] and check_bit[1] > check_bit[2] and statistics.mean(check_bit) < 200:
-			print('no card detected, skipped', check_bit, check_bit_x, check_bit_y)
+			#print('no card detected, skipped', check_bit, check_bit_x, check_bit_y)
 			continue
 
-		nominal = pytesseract.image_to_string(roi, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789AKQJ')[0].upper()
+		value = pytesseract.image_to_string(roi, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789AKQJ')[0].upper()
 
-		suit_bit_x = roi_x+6
-		suit_bit_y = roi_y+SYM_Y+4
+		# detect suit
+		roi_suit_x = roi_x + 2
+		roi_suit_y = roi_y + SYM_Y - 1
+		roi_suit = image[roi_suit_y:roi_suit_y+10,roi_suit_x:roi_suit_x+SYM_X-6]
+
+		bgr = [0, 0, 0]
+		pixels_counted = 0
+
+		for k in range(0, len(roi_suit)):
+			for j in range(0, len(roi_suit[0])):
+
+				m = np.mean(roi_suit[k][j])
+
+				if m < 230:
+					bgr[0] += roi_suit[k][j][0]
+					bgr[1] += roi_suit[k][j][1]
+					bgr[2] += roi_suit[k][j][2]
+					pixels_counted += 1
+
+		#pixels_count = len(roi_suit) * len(roi_suit[0])
+		bgr[0] /= pixels_counted
+		bgr[1] /= pixels_counted
+		bgr[2] /= pixels_counted
+		m = np.mean(bgr)
+
+		if bgr[2] > 243:
+			suit = 'h'
+		elif bgr[2] > 210:
+			suit = 'd'
+		elif m < 80.0:
+			suit = 's'
+		else:
+			suit = 'c'
+
+		#print('bgr', bgr, np.mean(bgr), value, suit)
+
+		#print( len(roi_suit), len(roi_suit[0]), len(roi_suit[0][0]) )
+
+		'''
+		if y >= 4:
+			roi_suit_y += 1
+
+		suit_bit_x = roi_x + 6
+		suit_bit_y = roi_y + SYM_Y + 4
 		suit_bit = image[suit_bit_y, suit_bit_x]
 		suit = 'b'
 		if suit_bit[2] > 200:
 			suit = 'r'
 			total_red_suits += 1
+		'''
 
-		#print( nominal, suit, x, y, roi_x, roi_y, check_bit, check_bit_x, check_bit_y )
-		print( nominal, suit, x, y, suit_bit, suit_bit_x, suit_bit_y )
-		if nominal not in CARD_NOMINALS_LIST:
-			print('failed to recongnize card nominal {} {}')
-			break
-		else:
+		#print( values, suit, x, y, roi_x, roi_y, check_bit, check_bit_x, check_bit_y )
+		#print( values, suit, x, y, suit_bit, suit_bit_x, suit_bit_y )
+		print( value, suit, x, y, bgr, m )
+
+		#if x == 3 and y == 5:
+		#	break
+
+		if value in RESTRICTED_VALUES_LIST:
+
+			#if {'value':value, 'suit':suit} in readed_cards_list:
+				#print('<<<<<<<<<<<<<<<<<<<<<<<')
+
 			total_cards_readed += 1
-			cards_dict[nominal] += 1
+			cards_dict[value] += 1
+			cards_suits_dict[suit] += 1
+			readed_cards_list += {'value':value, 'suit':suit}
+
+			# translate string to numeric values
+			numeric_value = 0
+			if value == '1':
+				numeric_value = 10
+			elif value == 'J':
+				numeric_value = 11
+			elif value == 'Q':
+				numeric_value = 12
+			elif value == 'K':
+				numeric_value = 13
+			elif value == 'A':
+				numeric_value = 1
+			else:
+				numeric_value = int(value)
+
+			# fill game lists
+			game[x].append({CARD_VALUE_KEY:numeric_value, CARD_SUIT_KEY:suit})
+		else:
+			print('failed to recongnize card values {} {}')
+			break
 
 	else:
 		continue
 	break
 
-if total_cards_readed != 52 or total_red_suits != 26:
-	print('readed wrong cards count {}, red_suits {}'.format(total_cards_readed, total_red_suits))
+if total_cards_readed != 52:
+	print('readed wrong cards count {}, red_suits {}'.format(total_cards_readed))
 
 for k,v in cards_dict.items():
 	if v != 4:
-		print('check readed nominals failed {}, {}'.format(k,v))
+		print('check readed values failed {}, {}'.format(k,v))
 
-#print(gray)
+for k,v in cards_suits_dict.items():
+	if v != 13:
+		print('check readed suits failed {}, {}'.format(k,v))
 
-#my_thresh()
-
-
-
-#hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-#h,s,v = cv2.split(hsv)
-#h[h<150]=0
-#h[h>180]=0
-
-# check zoomed letter
-# downscale
 
 show()
-exit(0)
-
-'''
-# remove green noise
-for row in range(1, len(image)):
-	for col in range(1, len(image[row])):
-		blue = image[row][col][0]
-		green = image[row][col][1]
-		red = image[row][col][2]
-		if green > max(blue, red) + 20:
-			image[row][col][0]  = 0
-			image[row][col][1] = 0
-			image[row][col][2] = 0
-'''
-
-#ret, bin = cv2.threshold(gray, 150, 255, cv2.ADAPTIVE_THRESH_MEAN_C)
-
-# no need blur?
-#gray = cv2.GaussianBlur(gray, (3, 3), 0)
-#edges = cv2.Canny(img, 60, 255)
-
-# getting gray channel
-#gray = cv2.split(img)
-
-#roi = img[0:1,0:1]
-
-#print( 'rows', len(gray[0]) )
-#print( 'columns', len(gray[0][0]) )
-#print( gray )
-#img1 = np.zeros((200,200,1), dtype=np.uint8)
-
-canny = cv2.Canny(bin, 1, 1)
-
-
-# Find my contours
-window_contours = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)[0]
-
-#Loop through my contours to find rectangles and put them in a list, so i can view them individually later.
-cntrRect = []
-for countour in window_contours:
-	epsilon = 0.05*cv2.arcLength(countour,True)
-	approx = cv2.approxPolyDP(countour,epsilon,True)
-	print('len approx {}'.format(len(approx)))
-	if len(approx) == 4:
-		#cv2.drawContours(roi,cntrRect,-1,(0,255,0),2)
-		#cv2.imshow('Roi Rect ONLY',roi)
-		cv2.drawContours(image, countour, -1, (0, 255, 0), 2)
-		#cv2.imshow('Roi Rect ONLY', img)
-		cntrRect.append(approx)
-
-
-# detect window
-
-# read game
-# read cards by top left 2 symbols
-
-# solve game
-# check random moves as stack of moves
-
-# execute solution
-
-
 print('ok')
